476fa5c904b0022ec5d261b1a8f0ebf8144d5737
Racheal-spec <folorunshotosin334@gmail.com>
2020-07-09 14:26:31 +0100

    adjusted alignnment

--- js/index.js
+++ js/index.js
@@ -1,16 +1,18 @@
 //Define Variables
 const shortenBtn = document.getElementById("shorten-btn");
 const linkResult =  document.getElementById('result');
+//const copyBtn = document.querySelector('.copy-btn');
 let newLinks;
 
 //Add eventlistener
 document.addEventListener("DOMContentLoaded", showsavedResults);
 shortenBtn.addEventListener("click", shortLink);
+//copyBtn.addEventListener("click", copyText);
 //Save results to local storage
 function saveResults(result) {
 let results;
 if(localStorage.getItem("results") === null ){
-  results = [];
+results = [];
 }else{
 results = JSON.parse(localStorage.getItem("results"));
 }
@@ -23,10 +25,10 @@ localStorage.setItem("results", JSON.stringify(results));
 function genUrl(URL){
 let protocols = URL.startsWith('http://')||URL.startsWith('https://')||URL.startsWith('ftp://');
 if(!protocols){
-  newLinks = "http://" + URL;
-  return newLinks;
+newLinks = "http://" + URL;
+return newLinks;
 } else{
-  return URL;
+return URL;
 }
 }
 // To Validate Url
@@ -35,7 +37,7 @@ return /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1
 }
 
 //Main function
-function shortLink(){
+function shortLink(event){
 event.preventDefault();
 
 let URL = document.getElementById('input-link').value;
@@ -48,64 +50,88 @@ saveResults(generateUrl);
 saveResults(linkResult.innerText)
 
 if (validUrl) {
-  
-  fetch('https://rel.ink/api/links/', {
-      method: 'POST',
-      body: JSON.stringify({'url':generateUrl}),
-      headers: {
-          'Content-Type': 'application/json'
-        },
-      mode: 'cors',
-    })
-    .then(response => response.json())
-    .then(result => {
-      document.getElementById('defaultUrl').innerHTML = generateUrl;
-      linkResult.innerHTML = '<a href="https://rel.ink/'+result.hashid+'">https://rel.ink/'+ result.hashid +'</a>';
-      document.querySelector('.copy-btn').innerHTML = "Copy";
-      console.log('Success:', result);
-    })
-    .catch(error => {
-      console.error('Error:', error);
-    });
+
+fetch('https://rel.ink/api/links/', {
+  method: 'POST',
+  body: JSON.stringify({'url':generateUrl}),
+  headers: {
+      'Content-Type': 'application/json'
+    },
+  mode: 'cors',
+})
+.then(response => response.json())
+.then(result => {
+  document.getElementById('defaultUrl').innerHTML = generateUrl;
+  linkResult.innerHTML = '<a href="https://rel.ink/'+result.hashid+'">https://rel.ink/'+ result.hashid +'</a>';
+  document.querySelector('.copy-btn').innerHTML = "Copy";
+  document.querySelector('.show-div').style.display = "block";
+  console.log('Success:', result);
+})
+.catch(error => {
+  console.error('Error:', error);
+});
 }else{
-  alert("Please input a valid Url!!"); 
+("Please input a valid Url!"); 
 } 
+
 }
+/*
+// Copy Button
+function copyButton(e) {
+
+e.target.innerText = 'Copied';
+document.querySelector('.copy-btn').innerHTML = "Copy";
+const url = e.target.previousElementSibling.className;
+console.log(url);
+copyText(url)
+}
+
+// Copy Text
+function copyText() {
+let copyText = document.getElementById("result");
+console.log(copyText);
+copyText.select();
+copyText.setSelectionRange(0, 99999);
+document.execCommand("copy");
+}
+*/
+
 //A function to make the saved results show on the interface
 
 function showsavedResults(result) {
 let results;
 if(localStorage.getItem("results") === null ){
-  results = [];
+results = [];
 }else{
 results = JSON.parse(localStorage.getItem("results"));
 }
 results.forEach(result => {
-  let URL = document.getElementById('input-link').value;
-  let generateUrl = genUrl(URL);
-  let validUrl = validateUrl(generateUrl);
+let URL = document.getElementById('input-link').value;
+let generateUrl = genUrl(URL);
+let validUrl = validateUrl(generateUrl);
 
-  if (validUrl) {
-    
-    fetch('https://rel.ink/api/links/', {
-        method: 'POST',
-        body: JSON.stringify({'url':generateUrl}),
-        headers: {
-            'Content-Type': 'application/json'
-          },
-        mode: 'cors',
-      })
-      .then(response => response.json())
-      .then(result => {
-        document.getElementById('defaultUrl').innerHTML = generateUrl;
-        linkResult.innerHTML= '<a href="https://rel.ink/'+result.hashid+'">https://rel.ink/'+ result.hashid +'</a>';
-        document.querySelector('.copy-btn').innerHTML = "Copy";
-        console.log('Success:', result);
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
-  }else{
-    alert("Please input a valid Url!!"); 
-  } 
+if (validUrl) {
+
+fetch('https://rel.ink/api/links/', {
+  method: 'POST',
+  body: JSON.stringify({'url':generateUrl}),
+  headers: {
+      'Content-Type': 'application/json'
+    },
+  mode: 'cors',
+})
+.then(response => response.json())
+.then(result => {
+  document.getElementById('defaultUrl').innerHTML = generateUrl;
+  linkResult.innerHTML= '<a href="https://rel.ink/'+result.hashid+'">https://rel.ink/'+ result.hashid +'</a>';
+  document.querySelector('.copy-btn').innerHTML = "Copy";
+  document.querySelector('.show-div').style.display = "block";
+  console.log('Success:', result);
+})
+.catch(error => {
+  console.error('Error:', error);
+});
+}else{
+("Please enter a valid URL")
+} 
 })};
